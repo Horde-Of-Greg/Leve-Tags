84141e1b811780c279b216d892d05e7e7a7bc462
Alessian Dormal <alessian@alessian.be>
2025-04-17 16:27:18 +0200

    Bro so many fucking lines added yet again
    Signed-off-by: Alessian Dormal <alessian@alessian.be>

--- tao/holyness_calc.js
+++ tao/holyness_calc.js
@@ -1,3 +1,5 @@
+if (!util.findUsers) util.findUsers = (ignored) => [msg.author];
+
 function getMap() {
   var mapArray = [];
   for (let i = 1; i < 6; i++) {
@@ -20,7 +22,18 @@ function calculateTotalWords(wordMap) {
   return total;
 }
 
+function isBonusWord(word) {
+  const bonusWords = ["tao", "hog", "pex"];
+  return bonusWords.includes(word);
+}
+
+function isBadWord(word) {
+  const badWords = ["conduits", "conduit"];
+  return badWords.includes(word);
+}
+
 function calculateHolyness(wordMap, message, totalWords) {
+  var extraWords = 0;
   var rawHolyness = 0;
   var words = message.toLowerCase().split(" ");
   const holynessCoefficient = (word) => {
@@ -30,14 +43,27 @@ function calculateHolyness(wordMap, message, totalWords) {
     return !rawNum ? 0 : bump - expo + 1;
   };
 
+  var holyness = 0;
+  var perfectHolyness = 0;
   words.forEach((word) => {
-    if (wordMap.has(word)) {
+    if (isBonusWord(word)) {
+      extraWords += 1;
+    } else if (isBadWord(word)) {
+      extraWords -= 1;
+    } else if (wordMap.has(word)) {
       rawHolyness += holynessCoefficient(word);
     }
   });
-  const firstKey = wordMap.keys().next().value;
-  const perfectHolyness = words.length * holynessCoefficient(firstKey);
-  const holyness = (rawHolyness / perfectHolyness) * 100;
+  if (extraWords > 0) {
+    holyness = 100;
+  } else if (extraWords === 0) {
+    const firstKey = wordMap.keys().next().value;
+    perfectHolyness = words.length * holynessCoefficient(firstKey);
+    holyness = (rawHolyness / perfectHolyness) * 100;
+  } else {
+    holyness = 0;
+  }
+
   if (holyness > 100) {
     return 100;
   }
@@ -78,12 +104,30 @@ if (msg.reference != null) {
   message1 = util.fetchMessages(msg.channel.messages.slice(-1)[0])[0];
 }
 const stopWords = util.fetchTag("tao_s_bcsw").body.split("\n");
+var message2 = undefined;
+try {
+  message2 = message1.content;
+} catch (e) {
+  msg.reply(
+    "Error: The message is too old or has been deleted. \n -Sincerely, God"
+  );
+}
+if (message2 == undefined) {
+  msg.reply(
+    "Error: The message is too old or has been deleted. \n -Sincerely, God"
+  );
+}
+var user = undefined;
+try {
+  user = util.findUsers(message1.authorId)[0];
+} catch (e) {
+  msg.reply(
+    "Error: The message is too old or has been deleted. \n -Sincerely, God"
+  );
+}
+
 message = excludeStopWords(
-  message1.cleanContent
-    .toLowerCase()
-    .replace(/\W+/g, " ")
-    .replace(/\s+/g, " ")
-    .split(" "),
+  message2.toLowerCase().replace(/\W+/g, " ").replace(/\s+/g, " ").split(" "),
   stopWords
 ).join(" ");
 
@@ -125,9 +169,9 @@ msg.reply({
         "https://cdn.discordapp.com/attachments/1266376637876928696/1362133906786357589/cat.png?ex=6801498e&is=67fff80e&hm=1ec6784bfdafb2856ccccf904b58e540340d1efc5e3f9229e7158ea6719dcc9b&",
     },
     color: messageColor(holyness),
-    title: "Holyness",
+    title: `${user.displayName}'s Holyness`,
     description:
-      "Your holyness is " +
+      `${user.displayName}'s holyness is ` +
       holyness.toFixed(2) +
       "%.\n" +
       messageConclusion(holyness) +
