f4e59b492f83e2374c785817a93b2a27b3cce8c6
Alessian Dormal <alessian@alessian.be>
2025-08-09 20:37:03 +0200

    Txt File Importing (#10)
    * add txt files importing. Maybe they should have a separate dir?
    
    * change gitignore to not ignore .txt

--- GH-Importer/importer.js
+++ GH-Importer/importer.js
@@ -5,21 +5,44 @@ const config = JSON.parse(fs.readFileSync(path.join(__dirname, './config.json'),
 const tagDump = JSON.parse(fs.readFileSync(path.join(__dirname, config.paths.rawDump), 'utf8'));
 const IDs = Object.keys(config.users);
 
+function removeFormattingBackticks(body) {
+    let output = body.replace(/```js/, '');
+    const lastIndex = output.lastIndexOf('```');
+    if (lastIndex !== -1) {
+        output = output.slice(0, lastIndex) + output.slice(lastIndex + 3);
+    }
+    return output;
+}
+
+function createDirs(absolutePath) {
+    if (!fs.existsSync(absolutePath)) {
+        fs.mkdirSync(absolutePath, { recursive: true });
+    }
+}
+
+function writeToFile(filePath, content) {
+    createDirs(path.dirname(filePath));
+    fs.writeFileSync(filePath, content);
+}
+
 tagDump.forEach(tag => {
     if (IDs.includes(tag.owner)) {
         const username = config.users[tag.owner];
-        if (tag.body.startsWith("```js") && !config.blacklist.includes(tag.name) && tag.hops.length === 1) {
-            let script = tag.body.replace(/```js/, '');
-            const lastIndex = script.lastIndexOf('```');
-            if (lastIndex !== -1) {
-                script = script.slice(0, lastIndex) + script.slice(lastIndex + 3);
-            }
+        if (config.blacklist.includes(tag.name) && tag.hops.length !== 1) {
+            return;
+        }
+        if (tag.body.startsWith("```js")) {
+
+            let script = removeFormattingBackticks(tag.body);
+
             const pathToFile = path.join(__dirname, config.paths.output, username, `${tag.name}.js`);
-            const dir = path.dirname(pathToFile);
-            if (!fs.existsSync(dir)) {
-                fs.mkdirSync(dir, { recursive: true });
+            writeToFile(pathToFile, script);
+        } else {
+            if (tag.name.length > 32) {
+                return;
             }
-            fs.writeFileSync(pathToFile, script);
+            const pathToFile = path.join(__dirname, config.paths.output, username, `${tag.name}.txt`);
+            writeToFile(pathToFile, tag.body);
         }
     }
 });
